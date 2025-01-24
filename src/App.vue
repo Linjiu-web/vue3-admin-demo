73729672e86dad82b7074e2a8c8d673a28db75c2
LinJiu <741802266@qq.com>
2025-01-24 14:25:33 +0800

    优化vite配置项

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
 
+const vvval = ref(0);
+const addVal = (x: number, y: number) => {
+  return vvval.value = x + y;
+};
+
+const setIconSvgStyle = computed(() => {
+	return `font-size: 48px;color: #000;`;
+});
 </script>
 
 <template>
-  <div>
-    这是后台管理系统1
-  </div>
+  <div>这是后台管理系统1->{{ vvval }}</div>
+  <button @click="addVal(10,66)">666</button>
+  <template>
+	<i class="el-icon" :style="setIconSvgStyle">
+		<component is="Histogram" />
+	</i>
+  <el-icon><Male /></el-icon>
+	<!-- <div v-else-if="isShowIconImg" :style="setIconImgOutStyle">
+		<img :src="getIconName" :style="setIconSvgInsStyle" />
+	</div>
+	<svg v-else-if="isShowLocalSvg" class="svg-icon icon" :style="setIconImgOutStyle">
+		<use :href="`#${getIconName}`" />
+	</svg>
+	<i v-else :class="getIconName" :style="setIconSvgStyle" /> -->
+</template>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,71 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import vueSetupExtend from "vite-plugin-vue-setup-extend";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  base: "./",
+  plugins: [
+    vue(),
+    vueSetupExtend(),
+    AutoImport({
+      imports: [
+        "vue", // 自动导入 Vue API，如 ref, reactive 等
+        "vue-router", // 自动导入 Vue Router API，如 useRouter, useRoute 等
+        "pinia", // 自动导入 Pinia 的 API，如 useStore
+      ],
+      dts: "./auto-imports.d.ts", // 自动生成 `auto-imports.d.ts` 声明文件
+      // 配置哪些文件会被扫描
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/, // .vue
+        /\.vue\?vue/, // .vue 文件中的 <script setup>
+      ],
+      // 自定义目录下的导入
+      dirs: [
+        "./src/composables", // 自动导入 `src/composables` 目录下的函数
+        "./src/stores", // 自动导入 `src/stores` 目录下的模块
+      ],
+      eslintrc: {
+        // 解决 eslint 报错问题
+        enabled: true, // 生成自动导入的 eslint 配置文件
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "192.168.31.159",
+    port: 8080, // 启动端口
+    open: false, // 启动后是否打开浏览器。建议关闭，首次打开的页面会热更新，否则每次都会打开新的tab页。
+    cors: true, // 允许跨域
+    // 本地代理接口
+    proxy: {
+      // dev环境
+      "/api": {
+        //TODO:更改为dev环境的后台服务地址
+        target: "http://192.168.31.174:37373/",
+        // target: "http://192.168.31.181:37008/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 重写路径
+      },
+      "/waiwang": {
+        //TODO:更改为dev环境的后台服务地址
+        target: "http://111.231.18.199:37008/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/waiwang/, ""), // 重写路径
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or "modern", "legacy"
+      },
+    },
+  },
+});
